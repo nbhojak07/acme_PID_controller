1abbf9eaafda384b000b1420af93f9fe719852a0
Justin Albrecht <jalbrec1@umd.edu>
2020-09-24 20:59:07 -0400

    Build with code coverage

--- test/pidControllerTest.cpp
+++ test/pidControllerTest.cpp
@@ -5,13 +5,13 @@
 TEST(computetest, zero_input) {
     pidController controller(1,1,1,1);
     double a = controller.Compute(0);
-    double b = 1;
+    double b = 0;
     EXPECT_EQ(b, a);
 }
 
 TEST(computetest, input_5) {
     pidController controller(1,1,1,1);
     double a = controller.Compute(5);
-    double b = 1;
+    double b = 0;
     EXPECT_EQ(b, a);
 }
